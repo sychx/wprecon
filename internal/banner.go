6c7a23fa6773da74bd30dc0123e45eff41c79070
Matheus <37225059+blkzy@users.noreply.github.com>
2020-12-08 00:07:04 -0300

    Update banner.go

--- internal/banner.go
+++ internal/banner.go
@@ -11,19 +11,19 @@ import (
 func SBanner(target string) {
 	Banner()
 	printer.Done("Target:", target)
-	printer.Done("Starting:", time.Now().Format(("02/jan/2006 15:04:05")))
+	printer.Done("Starting:", time.Now().Format(("02/jan/2006 15:04:05")), "\n")
 }
 
 // Banner :: A simple banner.
 func Banner() {
-	fmt.Print("——————————————————————————————————————————————————")
-	fmt.Printf(`
+	fmt.Print("——————————————————————————————————————————————————\n")
+	fmt.Print(`
 ___       _______________________________ 
 __ |     / /__  __ \_  ___/_  ____/_  __ \
 __ | /| / /__  /_/ /____ \_  / __ _  / / /
 __ |/ |/ / _  ____/____/ // /_/ / / /_/ / 
 ____/|__/  /_/     /____/ \____/  \____/  
-`)
+`, "\n")
 	fmt.Print("Github: ", "https://github.com/blackcrw/wpsgo\n")
 	fmt.Print("Version: ", "0.0.1a\n")
 	fmt.Print("——————————————————————————————————————————————————\n")
