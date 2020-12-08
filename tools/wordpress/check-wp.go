98172b03ed2c76f77175241f1151e3e98a136a5f
Matheus <37225059+blkzy@users.noreply.github.com>
2020-12-08 00:07:09 -0300

    Create check-wp.go

---
+++ tools/wordpress/check-wp.go
@@ -0,0 +1,84 @@
+package wps
+
+import (
+	. "fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/blkzy/wpsgo/pkg/gohttp"
+	"github.com/blkzy/wpsgo/pkg/printer"
+)
+
+func Check(target string) string {
+	var calc int
+	var exists int
+	var err error
+	var response gohttp.Response
+	var content []byte
+	var payloads = [...]string{
+		`<meta name="generator content="WordPress`,
+		`<a href="http://www.wordpress.com">Powered by WordPress</a>`,
+		`<link rel='https://api.w.org/'`}
+	var paths = [...]string{
+		"wp-content/uploads/",
+		"wp-content/plugins/",
+		"wp-content/themes/",
+		"wp-includes/",
+		"wp-admin/"}
+
+	wg.Add(2)
+
+	go func(URL string, htmlPayloads [3]string) {
+		response, err = gohttp.HttpRequest(gohttp.Http{URL: URL})
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		content, err = ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		for _, value := range htmlPayloads {
+			if strings.Contains(value, string(content)) {
+				exists++
+			}
+		}
+
+		wg.Done()
+	}(target, payloads)
+
+	go func(URL string, directories [5]string) {
+		for _, directory := range directories {
+			request, err := gohttp.HttpRequest(gohttp.Http{URL: URL + directory})
+
+			if err != nil {
+				printer.Fatal(err)
+			}
+
+			body, err := ioutil.ReadAll(request.Body)
+
+			if err != nil {
+				printer.Fatal(err)
+			}
+
+			if directory == "wp-admin/" && request.StatusCode == 200 || request.StatusCode == 403 {
+				printer.Warning("Status Code:", request.StatusCode, "in the URL:", URL+directory)
+				exists++
+			} else if !strings.Contains("Index Of", string(body)) {
+				printer.Done("Listing enable:", URL+directory)
+				exists++
+			}
+		}
+
+		wg.Done()
+	}(target, paths)
+
+	wg.Wait()
+
+	calc = exists / 8 * 100
+
+	return Sprintf("%v", calc)
+}
