885390dd43b6c39fc70b37cd09daf80d2d0a16d3
Matheus <37225059+blkzy@users.noreply.github.com>
2020-12-09 01:11:30 -0300

    Create wprecon.go

---
+++ cli/wprecon.go
@@ -0,0 +1,56 @@
+package cli
+
+import (
+	"os"
+
+	"github.com/blackcrw/wprecon/cli/cmd"
+	"github.com/blackcrw/wprecon/internal"
+	"github.com/blackcrw/wprecon/pkg/gohttp"
+	"github.com/blackcrw/wprecon/pkg/printer" // This is color lib
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "wprecon",
+	Short: "Wordpress Recon",
+	Long:  `wprecon (Wordpress Recon) is a scanner based on wpscan, only done in golang to get better performance!`,
+	Run: func(ccmd *cobra.Command, args []string) {
+		cmd.Wpcheck(ccmd)
+		cmd.Detectionwaf(ccmd)
+	},
+}
+
+// Execute ::
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		// printer.Danger(err)
+		os.Exit(0)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initBanner)
+
+	rootCmd.PersistentFlags().StringP("url", "u", "", "Target URL (Ex: http(s)://google.com/) "+printer.Required())
+	rootCmd.PersistentFlags().BoolP("detection-waf", "d", false, "")
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Verbosity")
+	rootCmd.PersistentFlags().BoolP("random-agent", "", false, "Use randomly selected HTTP(S) User-Agent header value")
+
+	rootCmd.MarkPersistentFlagRequired("url")
+}
+
+func initBanner() {
+	target, _ := rootCmd.Flags().GetString("url")
+	isURL, err := gohttp.IsValidURL(target)
+
+	switch isURL {
+	case false:
+		internal.Banner()
+		printer.Fatal(err)
+	case true:
+		internal.SBanner(target)
+	default:
+		internal.Banner()
+	}
+
+}
