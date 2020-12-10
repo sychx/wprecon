1744fbc6ac362a1f4e777cdca674b3b17c6cc6ca
blackcrw <37225059+blkzy@users.noreply.github.com>
2020-12-10 19:40:25 -0300

    Create wp-dir-files.go

---
+++ pkg/wordlist/wp-dir-files.go
@@ -0,0 +1,60 @@
+package wordlist
+
+var WPchangesLogs = [...]string {
+	"changelog.txt",
+	"changelog.md",
+	"CHANGELOG.txt",
+	"changelog",
+	"CHANGELOG.md",
+	"ChangeLog.txt",
+	"ChangeLog.md",
+	"CHANGELOG",
+}
+
+var WPfpd = [...]string{
+	"404.php",
+	"archive.php",
+	"author.php",
+	"comments.php",
+	"footer.php",
+	"functions.php",
+	"header.php",
+	"image.php",
+	"page.php",
+	"search.php",
+	"single.php",
+	"archive.php",
+}
+
+var WPlicense = [...]string{
+	"license.txt",
+	"license.md",
+	"LICENSE.md",
+	"LICENSE.txt",
+	"LICENSE",
+}
+
+var WPdirs = [...]string{
+	"js",
+	"css",
+	"images",
+	"inc",
+	"admin",
+	"src",
+	"widgets",
+	"lib",
+	"assets",
+	"includes",
+	"logs",
+	"vendor",
+	"core",
+}
+
+var WPreadme = [...]string{
+	"readme.txt",
+	"readme.md",
+	"README.md",
+	"README.txt",
+	"README",
+	"readme",
+}
