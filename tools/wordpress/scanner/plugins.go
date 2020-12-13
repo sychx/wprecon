56dd45d1074901f9b7c6ef86ea5992fb5e397514
blackcrw <37225059+blkzy@users.noreply.github.com>
2020-12-13 13:40:41 -0300

    Update plugins.go

--- tools/wordpress/scanner/plugins.go
+++ tools/wordpress/scanner/plugins.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"sync"
+	"time"
 
 	"github.com/blackcrw/wprecon/pkg/gohttp"
 	"github.com/blackcrw/wprecon/pkg/printer"
-	"github.com/blackcrw/wprecon/pkg/text"
 	"github.com/blackcrw/wprecon/pkg/wordlist"
 )
 
 type Plugins struct {
-	Verbose  bool
-	Request  gohttp.Http
-	response map[string]interface{}
+	Verbose bool
+	Request gohttp.Http
 }
 
-func (options *Plugins) Enumerate() (bool, []string) {
+func (options *Plugins) Enumerate() {
+	wg := new(sync.WaitGroup)
+
 	response, err := gohttp.HttpRequest(options.Request)
 
 	if err != nil {
@@ -34,25 +36,36 @@ func (options *Plugins) Enumerate() (bool, []string) {
 
 	submatchall := re.FindAllSubmatch([]byte(bodyBytes), -1)
 
-	var plugins []string
-	for _, plugin := range submatchall {
-		pluginString := fmt.Sprintf("%s", plugin[1])
+	set := make(map[string]int)
 
-		if _, has := text.ContainsSliceString(plugins, pluginString); !has {
-			plugins = append(plugins, pluginString)
+	for _, plugin := range submatchall {
+		plugin := fmt.Sprintf("%s", plugin[1])
+
+		set[plugin] += 1
+
+		if value, _ := set[plugin]; value <= 1 {
+			printer.Done("Plugin:", plugin)
+
+			if options.Verbose {
+				wg.Add(4)
+
+				go options.fullpathdisclosure(wg, plugin)
+				go options.changelog(wg, plugin)
+				go options.license(wg, plugin)
+				go options.readme(wg, plugin)
+
+				wg.Wait()
+
+			}
 		}
 	}
-
-	if len(plugins) != 0 {
-		return true, plugins
-	}
-
-	return false, plugins
 }
 
-func (options *Plugins) Changelog(plugin string) (bool, gohttp.Response) {
-	for _, value := range wordlist.WPchangesLogs {
-		options.Request.Dir = fmt.Sprintf("/wp-content/plugins/%s/%s", plugin, value)
+func (options *Plugins) fullpathdisclosure(wg *sync.WaitGroup, plugin string) {
+	defer wg.Done()
+
+	for _, value := range wordlist.WPfpd {
+		options.Request.Dir = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
 
 		response, err := gohttp.HttpRequest(options.Request)
 
@@ -67,9 +80,96 @@ func (options *Plugins) Changelog(plugin string) (bool, gohttp.Response) {
 		}
 
 		if response.StatusCode == 200 && string(bodyBytes) != "" {
-			return true, response
+			printer.Warning("Full Path Disclosure:", response.URLFULL)
+
+			break
 		}
 	}
 
-	return false, gohttp.Response{}
+	time.Sleep(time.Millisecond)
+}
+
+func (options *Plugins) readme(wg *sync.WaitGroup, plugin string) {
+	defer wg.Done()
+
+	for _, value := range wordlist.WPreadme {
+		options.Request.Dir = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
+
+		response, err := gohttp.HttpRequest(options.Request)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		if response.StatusCode == 200 && string(bodyBytes) != "" {
+			printer.Warning("Readme:", response.URLFULL)
+
+			break
+		}
+
+	}
+	time.Sleep(time.Millisecond)
+}
+
+func (options *Plugins) license(wg *sync.WaitGroup, plugin string) {
+	defer wg.Done()
+
+	for _, value := range wordlist.WPlicense {
+		options.Request.Dir = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
+
+		response, err := gohttp.HttpRequest(options.Request)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		if response.StatusCode == 200 && string(bodyBytes) != "" {
+			printer.Warning("License:", response.URLFULL)
+
+			break
+		}
+
+	}
+	time.Sleep(time.Millisecond)
+}
+
+func (options *Plugins) changelog(wg *sync.WaitGroup, plugin string) {
+	defer wg.Done()
+
+	for _, value := range wordlist.WPchangesLogs {
+		options.Request.Dir = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
+
+		response, err := gohttp.HttpRequest(options.Request)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			printer.Fatal(err)
+		}
+
+		if response.StatusCode == 200 && string(bodyBytes) != "" {
+			printer.Warning("Changelog:", response.URLFULL)
+
+			break
+		}
+
+	}
+
+	time.Sleep(time.Millisecond)
 }
