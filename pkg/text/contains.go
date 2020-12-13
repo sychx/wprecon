b5432c49d560a1f93941edc76ff06769617ee70b
blackcrw <37225059+blkzy@users.noreply.github.com>
2020-12-13 00:25:37 -0300

    Update contains.go
    And another commit for the code refactoring commits session.
    
    ✋

--- pkg/text/contains.go
+++ pkg/text/contains.go
@@ -7,9 +7,11 @@ import (
 func ContainsSliceString(slice []string, val string) (int, bool) {
 	for i, item := range slice {
 		if fmt.Sprintf("%s", item) == val {
+
 			return i, true
 		}
 	}
 
 	return -1, false
+
 }
