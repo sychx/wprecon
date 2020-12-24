4945db30fe6c954d5982d638464ab1d5f221d549
blackcrw <37225059+blkzy@users.noreply.github.com>
2020-12-24 16:12:44 -0300

    Update plugins.go
    Why did I remove the part that printed some plugin files ?!
    
    1. There is no need.
    2. Make several requests.
    3. Slows down the wprecon (even using goroutines).
    
    (Maybe in the future I will put it again, but in a faster way and with less requests.)

--- tools/wordpress/scanner/plugins.go
+++ tools/wordpress/scanner/plugins.go
@@ -4,177 +4,54 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
-	"sync"
-	"time"
 
 	"github.com/blackcrw/wprecon/pkg/gohttp"
 	"github.com/blackcrw/wprecon/pkg/printer"
-	"github.com/blackcrw/wprecon/pkg/wordlist"
 )
 
 // Plugins ::
 type Plugins struct {
 	HTTP    *gohttp.HTTPOptions
 	Verbose bool
-	wg      sync.WaitGroup
 }
 
 // Enumerate ::
 func (options *Plugins) Enumerate() {
-	plugins := func() map[string]bool {
-		request, err := gohttp.HTTPRequest(options.HTTP)
+	var pluginsMapper = make(map[string]bool)
 
-		if err != nil {
-			printer.Fatal(err)
+	request, err := gohttp.HTTPRequest(options.HTTP)
+
+	if err != nil {
+		printer.Fatal(err)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		printer.Fatal(err)
+	}
+
+	re := regexp.MustCompile("/wp-content/plugins/(.+?)/")
+
+	submatchall := re.FindAllSubmatch([]byte(bodyBytes), -1)
+
+	for _, plugin := range submatchall {
+		plugin := fmt.Sprintf("%s", plugin[1])
+
+		pluginsMapper[plugin] = true
+	}
+
+	if len(pluginsMapper) > 0 {
+		printer.Done("⎡ Plugin(s) :")
+	}
+
+	for plugin, _ := range pluginsMapper {
+		printer.Done("⎢", plugin)
+
+		if options.Verbose {
+			printer.Warning("—", "URL Path:", options.HTTP.URL.Simple+"wp-content/plugins/"+plugin)
 		}
-
-		bodyBytes, err := ioutil.ReadAll(request.Body)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		re := regexp.MustCompile("/wp-content/plugins/(.+?)/")
-
-		submatchall := re.FindAllSubmatch([]byte(bodyBytes), -1)
-
-		pluginsMapper := make(map[string]bool)
-
-		for _, plugin := range submatchall {
-			plugin := fmt.Sprintf("%s", plugin[1])
-
-			pluginsMapper[plugin] = true
-		}
-
-		return pluginsMapper
-	}()
-
-	options.wg.Add(1)
-	go func() {
-		if len(plugins) > 0 {
-			printer.Done("⎡ Plugin(s) :")
-		}
-
-		for plugin, _ := range plugins {
-			printer.Done("⎢", plugin)
-
-			if options.Verbose {
-<<<<<<< Updated upstream
-				printer.Warning("—", "URL Path:", options.HTTP.URL.Simple+"wp-content/plugins"+plugin)
-=======
-				printer.Warning("—", "URL Path:", options.HTTP.URL.Simple+"wp-content/plugins/"+plugin)
->>>>>>> Stashed changes
-				options.readme(plugin)
-				options.changelog(plugin)
-				// options.license(plugin)
-				// options.fullpathdisclosure(plugin)
-
-			}
-
-			time.Sleep(time.Millisecond)
-		}
-
-		defer options.wg.Done()
-	}()
-
-	options.wg.Wait()
+	}
 
 	printer.Println("")
-}
-
-func (options *Plugins) fullpathdisclosure(plugin string) {
-	for _, value := range wordlist.WPfpd {
-		options.HTTP.URL.Directory = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
-
-		response, err := gohttp.HTTPRequest(options.HTTP)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		if response.StatusCode == 200 || response.StatusCode == 406 && string(bodyBytes) != "" {
-			printer.Warning("— Full Path Disclosure:", response.URL.Full)
-
-			break
-		}
-	}
-}
-
-func (options *Plugins) readme(plugin string) {
-
-	for _, value := range wordlist.WPreadme {
-		options.HTTP.URL.Directory = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
-
-		response, err := gohttp.HTTPRequest(options.HTTP)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		if response.StatusCode == 200 || response.StatusCode == 406 && string(bodyBytes) != "" {
-			printer.Warning("— Readme:", response.URL.Full)
-
-			break
-		}
-	}
-}
-
-func (options *Plugins) license(plugin string) {
-	for _, value := range wordlist.WPlicense {
-		options.HTTP.URL.Directory = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
-
-		response, err := gohttp.HTTPRequest(options.HTTP)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		if response.StatusCode == 200 || response.StatusCode == 406 && string(bodyBytes) != "" {
-			printer.Warning("— License:", response.URL.Full)
-
-			break
-		}
-	}
-}
-
-func (options *Plugins) changelog(plugin string) {
-	for _, value := range wordlist.WPchangesLogs {
-		options.HTTP.URL.Directory = fmt.Sprintf("wp-content/plugins/%s/%s", plugin, value)
-
-		response, err := gohttp.HTTPRequest(options.HTTP)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			printer.Fatal(err)
-		}
-
-		if response.StatusCode == 200 || response.StatusCode == 406 && string(bodyBytes) != "" {
-			printer.Warning("— Changelog:", response.URL.Full)
-
-			break
-		}
-	}
 }
