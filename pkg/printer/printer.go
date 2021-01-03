5fb68686b380c5896eb6fb595150d728c77594dd
blackcrw <37225059+blkzy@users.noreply.github.com>
2021-01-03 12:44:26 -0300

    Update printer.go

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -3,13 +3,10 @@ package printer
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"os"
 	"strings"
 
 	color "github.com/logrusorgru/aurora" // This is color lib
-	"golang.org/x/net/html/charset"
 )
 
 // Required :: Constant with the word "required" in red.
@@ -18,21 +15,6 @@ var Required string = color.Red("(Required)").Bold().String()
 // Println ::
 func Println(text ...interface{}) {
 	fmt.Fprintln(os.Stdout, text...)
-}
-
-// Decode :: This function will convert any string to UTF-8.
-func Decode(text string) string {
-	r, err := charset.NewReader(strings.NewReader(text), "latin1")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(result)
 }
 
 // Danger ::
@@ -64,57 +46,6 @@ func Warning(text ...string) {
 	var prefix = color.Yellow("[!]").String()
 	var textString = strings.Join(text, " ")
 
-	_, err := io.WriteString(os.Stdout, prefix+" "+textString+"\n")
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-// Loading ::
-func Loading(text ...string) {
-	var prefix = color.Yellow("[✲]").String()
-	var textString = strings.Join(text, " ")
-
-	_, err := io.WriteString(os.Stdout, prefix+" "+textString+"\n")
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-// LoadingDone ::
-func LoadingDone(text ...string) {
-	var prefix = color.Green("[✔]").String()
-	var textString = strings.Join(text, " ")
-
-	fmt.Print("\033[G\033[K")
-	_, err := io.WriteString(os.Stdout, prefix+" "+textString+"\n")
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-// LoadingDanger ::
-func LoadingDanger(text ...string) {
-	var prefix = color.Red("[✗]").String()
-	var textString = strings.Join(text, " ")
-
-	fmt.Print("\033[G\033[K")
-	_, err := io.WriteString(os.Stdout, prefix+" "+textString+"\n")
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-// LoadingWarning ::
-func LoadingWarning(text ...string) {
-	var prefix = color.Yellow("[!]").String()
-	var textString = strings.Join(text, " ")
-
-	fmt.Print("\033[G\033[K")
 	_, err := io.WriteString(os.Stdout, prefix+" "+textString+"\n")
 
 	if err != nil {
