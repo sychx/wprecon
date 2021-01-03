a65b615a3d6182fea81876982bf9193d0b1b35b1
blackcrw <37225059+blkzy@users.noreply.github.com>
2021-01-03 18:41:46 -0300

    Update helper.go

--- internal/pkg/banner/helper.go
+++ internal/pkg/banner/helper.go
@@ -1,10 +1,13 @@
 package banner
 
-// Help ::
-var Help = `wprecon (Wordpress Recon) is a scanner based on wpscan, only done in golang to get better performance!
+// HelpMain ::
+var HelpMain = `wprecon (Wordpress Recon) is a scanner based on wpscan, only done in golang to get better performance!
 
 Usage:
   wprecon [flags]
+
+Commands:
+  fuzzer, fuzz
 
 Flags:
   -u, --url string           Target URL (Ex: http(s)://example.com/). (Required)
@@ -13,11 +16,29 @@ Flags:
       --users-enumerate      Use the supplied mode to enumerate Users.
       --themes-enumerate     Use the supplied mode to enumerate Themes.
       --plugins-enumerate    Use the supplied mode to enumerate Plugins.
-      --fuzzer-backup        Performs a fuzzing to try to find the backup file if it exists.
       --no-check-wp          Will skip wordpress check on target.
       --random-agent         Use randomly selected HTTP(S) User-Agent header value.
-      --tor                  Use Tor anonymity network
+      --tor                  Use Tor anonymity network.
       --disable-tls-checks   Disables SSL/TLS certificate verification.
-  -h, --help                 help for wprecon
+  -h, --help                 help for wprecon.
+  -v, --verbose              Verbosity mode.
+`
+
+// HelpFuzzer ::
+var HelpFuzzer = `wprecon (Wordpress Recon) is a scanner based on wpscan, only done in golang to get better performance!
+
+Usage:
+  wprecon fuzzer [flags]
+
+Flags:
+      --backup-file        Performs a fuzzing to try to find the backup file if it exists.
+  -h, --help                 help for wprecon.
+
+Global Flags:
+  -u, --url string           Target URL (Ex: http(s)://example.com/). (Required)
+      --random-agent         Use randomly selected HTTP(S) User-Agent header value.
+      --tor                  Use Tor anonymity network.
+      --disable-tls-checks   Disables SSL/TLS certificate verification.
+  -h, --help                 help for wprecon.
   -v, --verbose              Verbosity mode.
 `
