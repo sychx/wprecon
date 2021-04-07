60963f0bffa13aaac0118a49cae2f1a2766b72ea
blackcrw <blackzacky@protonmail.com>
2021-04-07 19:13:00 -0300

    update: 2 functions create
    Both functions were made to supply the need to do searches within a slice by values.

--- internal/pkg/text/contains.go
+++ internal/pkg/text/contains.go
@@ -1,34 +1,58 @@
 package text
 
-// ContainsInSlice :: This function will be used to check if a word/text already exists within a slice.
-func ContainsInSlice(slice []string, value string) (int, bool) {
+func FindStringInSliceSlice(slice [][]string, key int, value string) (int, bool) {
 	for i, item := range slice {
-		if item == value {
+		if value == item[key] {
 			return i, true
 		}
 	}
 
-	return 0, false
+	return -1, false
+}
+
+func FindByValueInIndex(slice [][]string, sub string) int {
+	for i, item := range slice {
+		for _, item2 := range item {
+			if sub == item2 {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
+
+// ContainsInSlice :: This function will be used to check if a word/text already exists within a slice.
+func ContainsInSlice(slice []string, value string) (int, bool) {
+	for i, item := range slice {
+		if value == item {
+			return i, true
+		}
+	}
+
+	return -1, false
 }
 
 // ContainsInSliceSlice :: This function will be used to check if a word/text already exists within a slice.
 func ContainsInSliceSlice(slice [][]string, value string) (int, bool) {
 	for i, item := range slice {
-		if item[0] == value {
-			return i, true
+		for _, item2 := range item {
+			if value == item2 {
+				return i, true
+			}
 		}
 	}
 
-	return 0, false
+	return -1, false
 }
 
 // ContainsInSliceOfMap :: This function will be used to check if a word/text already exists within a Map.
 func ContainsInSliceOfMap(slice []map[string]string, sub string, s string) (int, bool) {
 	for i, item := range slice {
-		if item[sub] == s {
+		if s == item[sub] {
 			return i, true
 		}
 	}
 
-	return 0, false
+	return -1, false
 }
