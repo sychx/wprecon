1495ad9d3aebdef19323ffa30c719f312eb81455
blackcrw <blackzacky@protonmail.com>
2021-04-17 07:42:20 -0300

    update: news features

--- tools/wordpress/enumerate/plugins.go
+++ tools/wordpress/enumerate/plugins.go
@@ -1,12 +1,12 @@
 package enumerate
 
 import (
-	"fmt"
 	"regexp"
 	"sync"
 
 	"github.com/blackbinn/wprecon/internal/pkg/text"
 	"github.com/blackbinn/wprecon/tools/wordpress/commons"
+	"github.com/blackbinn/wprecon/tools/wordpress/extensions"
 )
 
 const (
@@ -16,25 +16,29 @@ const (
 )
 
 type plugin struct {
-	raw           string
-	wpContentPath string
-	plugins       [][]string
+	target, raw, wpContentPath                  string
+	plugins                                     [][]string
+	CountPluginsPassive, CountPluginsAggressive int
 }
 
-func NewPlugins(raw string, wpcontent string) *plugin {
-	return &plugin{raw: raw, wpContentPath: wpcontent}
+func NewPlugins(target, raw, wpcontent string) *plugin {
+	return &plugin{target: target, raw: raw, wpContentPath: wpcontent}
 }
 
 // Passive :: "How does passive enumeration work?"
 // We took the source code of the index that was saved in memory and from there we do a search using the regex.
 func (object *plugin) Passive() [][]string {
 	var re = regexp.MustCompile(object.wpContentPath + MatchPluginPassive)
+	var submatch = re.FindAllStringSubmatch(object.raw, -1)
 
-	return re.FindAllStringSubmatch(object.raw, -1)
+	object.CountPluginsPassive = len(submatch)
+
+	return submatch
 }
 
 func (object *plugin) Aggressive() [][]string {
 	var wg sync.WaitGroup
+	var mx sync.Mutex
 
 	wg.Add(2)
 
@@ -47,11 +51,13 @@ func (object *plugin) Aggressive() [][]string {
 					If the condition is true, it means that the plugin exists. Soon he will not add the list, but the condition will go to the else that will add another match for the plugins.
 					Note: For you to understand better, I recommend that you see this in operation.
 				*/
+				mx.Lock()
 				if _, contains := text.FindStringInSliceSlice(object.plugins, 1, submatch[1]); !contains {
 					object.plugins = append(object.plugins, submatch)
 				} else if index := text.FindByValueInIndex(object.plugins, submatch[1]); index != -1 {
-					object.plugins[index][0] = fmt.Sprintf("%s,%s", object.plugins[index][0], submatch[0])
+					object.plugins[index][0] = object.plugins[index][0] + "," + submatch[0]
 				}
+				mx.Unlock()
 			}
 		}
 
@@ -60,18 +66,40 @@ func (object *plugin) Aggressive() [][]string {
 
 	go func() {
 		for _, submatch := range object.Passive() {
+			mx.Lock()
 			if _, has := text.ContainsInSliceSlice(object.plugins, submatch[1]); !has {
 				object.plugins = append(object.plugins, submatch)
 			} else if index := text.FindByValueInIndex(object.plugins, submatch[1]); index != -1 {
-				object.plugins[index][0] = fmt.Sprintf("%s,%s", object.plugins[index][0], submatch[0])
-				object.plugins[index][2] = fmt.Sprintf("%s,%s", object.plugins[index][2], submatch[2])
+				object.plugins[index][0] = object.plugins[index][0] + "," + submatch[0]
+				object.plugins[index][2] = object.plugins[index][2] + "," + submatch[2]
 			}
+			mx.Unlock()
 		}
 
 		defer wg.Done()
 	}()
 
 	wg.Wait()
+
+	for _, plugin := range object.plugins {
+		var path = object.wpContentPath + "/plugins/" + plugin[1] + "/"
+
+		if match, version := extensions.GetVersionByIndexOf(object.target, path); version != "" {
+			plugin[0] = plugin[0] + "," + match
+			plugin[2] = plugin[2] + "," + version
+		} else if match, version := extensions.GetVersionByReadme(object.target, path); version != "" {
+			plugin[0] = plugin[0] + "," + match
+			plugin[2] = plugin[2] + "," + version
+		} else if match, version := extensions.GetVersionByChangeLogs(object.target, path); version != "" {
+			plugin[0] = plugin[0] + "," + match
+			plugin[2] = plugin[2] + "," + version
+		} else if match, version := extensions.GetVersionByReleaseLog(object.target, path); version != "" {
+			plugin[0] = plugin[0] + "," + match
+			plugin[2] = plugin[2] + "," + version
+		}
+	}
+
+	object.CountPluginsAggressive = len(object.plugins)
 
 	return object.plugins
 }
